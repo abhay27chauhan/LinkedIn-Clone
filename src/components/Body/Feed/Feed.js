db8b46d88c0c9d06b9dea0c138bc3720d98e5428
Abhay Chauhan <chauhan27.abhay@gmail.com>
2021-02-13 12:51:25 +0530

    feed and widgets section completed

--- src/components/Body/Feed/Feed.js
+++ src/components/Body/Feed/Feed.js
@@ -1,10 +1,32 @@
-import React from 'react'
-import './Feed.css'
+import React, { useState } from 'react';
+import './Feed.css';
+import CreateIcon from '@material-ui/icons/Create';
+import ImageIcon from '@material-ui/icons/Image';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import EventNoteIcon from '@material-ui/icons/EventNote';
+import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
+import InputOption from './InputOption';
 
 function Feed() {
+    const [input, setInput] = useState('');
+
     return (
         <div className="feed">
-            
+            <div className="feed_inputContainer">
+                <div className="feed_input">
+                    <CreateIcon />
+                    <form>
+                        <input value={input} onChange={e => setInput(e.target.value)} type="text" />
+                        <button type="submit">Send</button>
+                    </form>
+                </div>
+                <div className="feed_inputOption">
+                    <InputOption Icon={ImageIcon} title="Photo" color="#70b5f9" />
+                    <InputOption Icon={SubscriptionsIcon} title="Video" color="#7E7A33E" />
+                    <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD" />
+                    <InputOption Icon={CalendarViewDayIcon} title="Write article" color="#7FC15E" />
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Body/Widgets/Widgets.js
+++ src/components/Body/Widgets/Widgets.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import './Widgets.css'
+
+import InfoIcon from '@material-ui/icons/Info';
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+
+import './Widgets.css';
+import { IconButton } from '@material-ui/core';
 
 function Widgets() {
+    const newsArticle = (heading, subtitle) => (
+        <div className="widget_article">
+        <div className="widgets_articleleft">
+            <FiberManualRecordIcon />
+        </div>
+
+        <div className="widgets_articleright">
+            <h4>{heading}</h4>
+            <p>{subtitle}</p>
+        </div>
+        </div>
+    );
+    
     return (
         <div className="widgets">
-            
+            <div className="widget_header">
+                <h2>LinkedIn News</h2>
+                <IconButton >
+                    <InfoIcon />
+                </IconButton>
+            </div>
+
+            {newsArticle("Coronavirus: Lagos updates", "Tops news - 324 readers")}
+            {newsArticle("Bitcoin hits new high", "Tops news - 2,324 readers")}
         </div>
     )
 }
